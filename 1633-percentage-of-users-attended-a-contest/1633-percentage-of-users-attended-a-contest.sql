e81ef689725313661a380020e7d6d8ec573c9a9e
Charchit sharma <2023pietcdcharchit010@poornima.org>
2026-08-12 23:20:05 +0530

    Time: 1057 ms (49.18%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,6 @@
 SELECT R.contest_id,ROUND(COUNT(R.user_id) * 100.0 / (SELECT COUNT(*) FROM Users),2) AS percentage
 FROM Register AS R
+INNER JOIN Users AS U
+ON R.user_id = U.user_id
 GROUP BY R.contest_id
 ORDER BY percentage DESC, R.contest_id ASC;
